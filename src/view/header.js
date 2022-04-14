fcad65d866b1b4f93de5b67e52419412e4d307a7
l050101 <l01062506156@gmail.com>
2022-04-14 18:16:37 +0900

    end header

--- src/view/header.js
+++ src/view/header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./style/header.css"
-import { Widgets, Menu, Search} from '@mui/icons-material';
+import { AiOutlineSearch,AiOutlineMenu } from "react-icons/ai";
+import { IoPartlySunny } from "react-icons/io5";
 const Header = () => {
     return(
         <div className="header">
             <div className="left">
-                <Search/>
+                <ul>
+                    <li className="localName">Useong</li>
+                    <li className="nowTab">news</li>
+                    <li className="nowDate">2022 / 04 / 07</li>
+                    <li className="nowTime">13 : 36 : 45</li>
+                    <li> <IoPartlySunny className="icon"/> </li>
+                </ul>
             </div>
-
-            <Widgets className=""/>
-            <Menu/>
-            <p>아이콘</p>
+            <div>
+                <AiOutlineMenu className="lightIcon icon" onClick={()=>{console.log("click Menu!")}} />
+                <AiOutlineSearch className="lightIcon icon" onClick={()=>{console.log("click Search")}}/>
+            </div>
         </div>
     )
 }
